8d92601f101f0144f85aea4418f236e0c557a3aa
Свистунова Анастасия <svistunova7910@gmail.com>
2026-09-17 15:41:45 +0300

    Task3: уникальные элементы без LINQ

--- Task3.Unique/Program.cs
+++ Task3.Unique/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿uusing System;
+
+namespace Task3.Unique
+{
+    class Program
+    {
+        static bool IsExists(int[] a, int b)
+        {
+            foreach(int el in a)
+            if (a==b) return true;
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr=new int[10];
+            Random random=new Random();
+            for(int i=0;i<arr.Lenght;i++)
+            arr[i]=random.Next(1,6);
+            System.Console.WriteLine(string.join(", ", arr));
+            int j=0;
+            int[] t=new int[0];
+            foreach(int el in arr)
+            if (!IsExists(t, el))
+            {
+                Array.Resize(ref t,j+1); //Вот так не хорошо!!!
+                t[j]=el;
+                j++;
+            }
+            System.Console.WriteLine(string.Join(", ", t));
+        }
+    }
+}
